431a56b2c08cabd62efedb6a59d5fe87698ed675
oudijk <Oudijk16@hotmail.com>
2026-06-19 02:26:32 -0400

    woops forgot \n for new lines, silly me

--- js/ip.js
+++ js/ip.js
@@ -14,7 +14,7 @@ async function getVisitorData() {
             const ispValue = data.org || "UNKNOWN_ISP";
 
             const fullString =
-            `IP: ${ipValue}\nLOC: ${cityValue}\nISP: ${ispValue}`;
+            `\nIP: ${ipValue}\nLOC: ${cityValue}\nISP: ${ispValue}`;
             typeEffect(targetElement, ":)", 120);
 
             setTimeout(() => {
